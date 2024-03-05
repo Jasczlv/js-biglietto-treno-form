18335bb6a28061f609087cd1bf532e183c0cf633
Jasczlv <ianis.cozlov@gmail.com>
2024-03-05 19:46:35 +0100

    ADD tanto codice js

--- js/app.js
+++ js/app.js
@@ -5,17 +5,44 @@
 // oppure applicato uno sconto del 40% per gli over 65.
 // L’output del prezzo finale va stampato in console in forma “umana” cioè con al massimo due decimali, per indicare centesimi sul prezzo. Questo richiederà un minimo di ricerca.
 
-// CHIEDERE IL NUMERO DI KM DA PERCORRERE ED ETA'
+// CHIEDERE IL NOME E COGNOME
+const getNomeCognome = document.getElementById('nome-cognome'); //?
+const nomeCognome = getNomeCognome.value; //stringa
 
-const inputKm = document.getElementById('km'); // stringa
-const inputEta = document.getElementById('age'); // strina
-const submitElement = document.getElementById('submit')
 
+//REGISTRARE IL SUBMIT
+const submitElement = document.getElementById('submit');//?
+
+
+//seleziona sezione di biglietto e dati passeggero nel html
+const biglietto = document.querySelector('.biglietto')
+const datiPasseggero = document.querySelector('div.biglietto .col')
+
+
+//funzione da svolgersi al click sul pulsante
 submitElement.addEventListener('click', function (){ 
 
-    km = inputKm.value
-    eta = inputEta.value
-    console.log(km + ' km',eta)
+    // inserisce nome e cognome all'interno del HTML
+
+    datiPasseggero.innerHTML += `
+    <div>
+        <span>${nomeCognome}</span>
+    </div>
+    <br>
+    `
+
+    //prende le variabili KM ed ETA' dal HTML
+
+    const inputKm = document.getElementById('km'); // stringa
+    const inputEta = document.getElementById('age'); // stringa
+   
+    //rende le variabili number
+
+    km = parseInt(inputKm.value); // number
+    eta = parseInt(inputEta.value); // number
+
+    //stampa le due variabili 
+    console.log(km + ' km',eta); // stringa
 
     // DEFINIRE COEFFICIENTE DEL PREZZO PER KM 
 
@@ -24,25 +51,68 @@ submitElement.addEventListener('click', function (){
     // CALCOLO PREZZI BIGLIETTI
 
     let prezzoBiglietto = parseFloat(km * coeffKm);
+    const offerta = document.querySelector('div.biglietto .col:nth-child(3)')
+    const displayPrezzo = document.querySelector('div.biglietto .col:nth-child(5)')
+
 
     // SCONTO MINORENNI 
 
-    if (eta = 'minorenne'){
+    //Se minore di 18 allora
+    if (eta < 18){
+
+        //calcola prezzo biglietto minori e printa
         let scontoMinori = (prezzoBiglietto - (prezzoBiglietto * (20/100)));
         console.log('Prezzo minori ' + scontoMinori.toFixed(2));
+
+        //inserisce tipologia di offerta all'interno del HTML
+        offerta.innerHTML += `
+        <span> Prezzo Minori </span>
+        `
+
+        //inserisce prezzo scontato nel HTML
+        displayPrezzo.innerHTML += `
+        <span>${scontoMinori.toFixed(2)}</span>
+        `
+
     }
 
     // SCONTO PER OVER 65
 
-    else if (eta = 'over'){
+    //Se maggiore di 65 allora
+    else if (eta > 65){
+
+        //calcola prezzo biglietto over 65 e printa
         let scontoOver = (prezzoBiglietto - (prezzoBiglietto * (40/100)));
         console.log('Prezzo over 65 ' + scontoOver.toFixed(2));
+
+        //inserisce tipologia di offerta all'interno del HTML
+        offerta.innerHTML += `
+        <span> Prezzo Anziani </span>
+        `
+
+        //inserisce prezzo scontato nel HTML
+        displayPrezzo.innerHTML += `
+        <span>${scontoOver.toFixed(2)}</span>
+        `
     }
 
     // PREZZO NON SCONTATO
 
+    //Se nessuna delle due citate sopra allora
     else {
+
+        //printa prezzo standard
         console.log('Prezzo intero ' + prezzoBiglietto.toFixed(2));
+
+        //inserisce tipologia di offerta all'interno del HTML (nessuna in questo caso)
+        offerta.innerHTML += `
+        <span> Prezzo Standard </span>
+        `
+
+        //inserisce prezzo NON scontato nel HTML
+        displayPrezzo.innerHTML += `
+        <span>${prezzoBiglietto.toFixed(2)}</span>
+        `
     }
 })
 
